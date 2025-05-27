54eed59f9201ec59305756ffd52c262a67b5a58b
neutrall85 <canni85@mail.ru>
2025-05-27 17:28:27 +0300

    fix TestDiffer.java

--- app/src/test/java/hexlet/code/TestDiffer.java
+++ app/src/test/java/hexlet/code/TestDiffer.java
@@ -6,7 +6,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class TestDiffer {
 
 
-    private static final String expected = "{\n"
+    private static final String EXPECTED = "{\n"
             + "  - follow: false\n"
             + "    host: hexlet.io\n"
             + "  - proxy: 123.234.53.22\n"
@@ -20,13 +20,13 @@ public class TestDiffer {
         String pathFirstFile = "file1.json";
         String pathSecondFile = "file2.json";
         String actual = Differ.generate(pathFirstFile, pathSecondFile);
-        assertThat(actual).isEqualTo(expected);
+        assertThat(actual).isEqualTo(EXPECTED);
     }
     @Test
     public void testDifferGenerateYAML() throws Exception {
         String pathFirstFile = "file1.yml";
         String pathSecondFile = "file2.yml";
         String actual = Differ.generate(pathFirstFile, pathSecondFile);
-        assertThat(actual).isEqualTo(expected);
+        assertThat(actual).isEqualTo(EXPECTED);
     }
 }
